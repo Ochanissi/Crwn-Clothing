07f5628b2ca0d7146ddeaaa483192a07dadc07ed
Mirel Home <mirelalex2094@gmail.com>
2020-04-29 19:55:37 +0300

    fixed a bug and updated

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ class SignIn extends React.Component {
             type='email'
             value={this.state.email}
             handleChange={this.handleChange}
-            label='email'
+            label='Email'
             required
           />
           <FormInput
@@ -55,7 +55,7 @@ class SignIn extends React.Component {
             type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label='password'
+            label='Password'
             required
           />
 
